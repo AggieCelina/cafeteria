d061b51285e11b20ed783432c02410e93985f81f
AggieCelina <agnieszka.c.markiewicz@gmail.com>
2016-09-18 17:08:31 +0200

    layout ok and working ImageButtons

--- app/src/main/java/aggiecelina/cafeteria/MainActivity.java
+++ app/src/main/java/aggiecelina/cafeteria/MainActivity.java
@@ -15,10 +15,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     int coffee_price = 5;
+    int coffee_2_price = 10;
+    int ice_price = 15;
+    int tea_price = 20;
     int total_counter = 0;
 
 
-    public void showBasket(View view){
+
+    public void selectCoffee(View view) {
 
         TextView basket = (TextView) findViewById(R.id.basket);
         total_counter = total_counter + coffee_price;
@@ -26,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void selectCoffee2(View view) {
+
+        TextView basket = (TextView) findViewById(R.id.basket);
+        total_counter = total_counter + coffee_2_price;
+        basket.setText("" + total_counter);
+    }
+
+    public void selectIce(View view) {
+
+        TextView basket = (TextView) findViewById(R.id.basket);
+        total_counter = total_counter + ice_price;
+        basket.setText("" + total_counter);
+    }
+
+    public void selectTea(View view) {
+
+        TextView basket = (TextView) findViewById(R.id.basket);
+        total_counter = total_counter + tea_price;
+        basket.setText("" + total_counter);
+    }
+
 
 
 }
